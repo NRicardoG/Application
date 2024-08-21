15f406bc1a7d9987209ec5adbc76431e610c165b
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-08-20 19:14:50 -0500

    lector y carro
    Estan los archivos .dart en donde se puede habrir el lector, leer el codigo y hacer el request a la API conectada

---
+++ micro_tech_application/lib/escaneo_producto/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lector_facturacion/carro_compra/carrito_page.dart';
+import 'package:lector_facturacion/escaneo_producto/home_page_controller.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage() {
+    Get.put(HomePageController());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Facturación'),
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Escaner productos',
+              style: Get.textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+            GetBuilder<HomePageController>(
+              builder: (controller) {
+                return Text(
+                  controller.valorCodigoBarras,
+                  style: Get.theme.textTheme.headlineMedium,
+                );
+              },
+            ),
+            SizedBox(height: 10),
+            TextButton.icon(
+              icon: Image.asset(
+                'assets/icon.png',
+                width: 50,
+              ),
+              label: Text(
+                'Escanear Codigo',
+                style: Get.theme.textTheme.headlineMedium,
+              ),
+              onPressed: () => Get.find<HomePageController>().escanearCodigoBarras(context),
+            ),
+            SizedBox(height: 20),
+            TextButton.icon(
+              icon: Icon(Icons.shopping_cart),
+              label: Text(
+                'Ver Carrito',
+                style: Get.theme.textTheme.headlineMedium,
+              ),
+              onPressed: () => Get.to(CarritoPage()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
