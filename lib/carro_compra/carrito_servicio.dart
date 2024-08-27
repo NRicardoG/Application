70f704b328b5aafe7cf8901bafd7f43664134836
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-08-27 12:04:44 -0500

    correccion carpetas

--- lib/carro_compra/carrito_servicio.dart
+++ lib/carro_compra/carrito_servicio.dart
@@ -17,35 +17,22 @@ class CarritoService extends GetxService {
     );
     pr.show();
 
-    final url =
-        'https://world.openfoodfacts.org/api/v2/product/${codigoProducto}.json';
+    final url = 'http://192.168.1.10:3000/product/${codigoProducto}';
     final response = await http.get(Uri.parse(url));
+    final data = jsonDecode(response.body);
 
     if (response.statusCode == 200) {
       // If the server returns a 200 OK response, parse the JSON.
-      final data = jsonDecode(response.body);
+      data['IMAGE'] = 'http://192.168.1.10:3000/${data['IMAGE']}';
+      Map<String, dynamic> nuevoProducto = data;
 
-      if (data['status'] == 0) {
-        Get.snackbar('Error', 'Producto no encontrado');
-      } else {
-        var nombreProducto = data['product']['product_name'] ??
-            data['product']['generic_name'] ??
-            'Nombre no encontrado';
+      // Agregar mapas a la lista
+      productos.add(nuevoProducto);
 
-        Map<String, dynamic> nuevoProducto = {
-          'id': codigoProducto,
-          'nombre': nombreProducto,
-          'urlImagen': data['product']['image_front_small_url'],
-        };
-
-        // Agregar mapas a la lista
-        productos.add(nuevoProducto);
-
-        Get.snackbar('Codigo Escaneado', nombreProducto);
-      }
+      Get.snackbar('Producto Escaneado', data['NAME']);
     } else {
       // If the server did not return a 200 OK response, throw an exception.
-      Get.snackbar('Error', 'Error al escanear el código de barras');
+      Get.snackbar('Error', data['status']);
     }
     pr.hide();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-<<<<<<< Updated upstream
 import 'package:lector_facturacion/carro_compra/carrito_servicio.dart';
 import 'package:lector_facturacion/escaneo_producto/home_page.dart';
-
-=======
-import 'package:lector_facturacion/carrito_servicio.dart';
-import 'package:lector_facturacion/home_page.dart';
->>>>>>> Stashed changes
 
 
 void main() {
