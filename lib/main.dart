51acf136b6cb129acfa86f6443a47e2849fce73b
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-08-27 12:00:29 -0500

    actualizacion api

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+<<<<<<< Updated upstream
 import 'package:lector_facturacion/carro_compra/carrito_servicio.dart';
 import 'package:lector_facturacion/escaneo_producto/home_page.dart';
 
+=======
+import 'package:lector_facturacion/carrito_servicio.dart';
+import 'package:lector_facturacion/home_page.dart';
+>>>>>>> Stashed changes
 
 
 void main() {
